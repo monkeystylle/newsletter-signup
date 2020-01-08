7ad1ac58daa7db039c8af5638ad5a5a157fe8e0a
Joseph Brummel Bayangos <monkeystylle@gmail.com>
2020-01-08 00:54:54 -0800

    Setting up Newsletter page

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const request = require('request');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.get('/', (req,res) => {
+    res.sendFile(__dirname + '/signup.html');
+});
+
+app.post('/', (req,res) => {
+    let firstName = req.body.fName;
+    let lastName = req.body.lName;
+    let email = req.body.email;
+
+    console.log(firstName, lastName, email);
+});
+
+
+
+app.listen(3000, () => {
+    console.log('server is running on port 3000');
+});
+
